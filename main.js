c303ee29e110bc40e665a3d8a0022e30a7251f99
really-riel <110791013+really-riel@users.noreply.github.com>
2022-12-07 15:01:15 +0100

    new

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const searchBtn = document.querySelector(".search_btn");
 const searchInput = document.querySelector(".search_input");
 const movieContainer = document.querySelector(".movies_container");
+const searchResult = document.querySelector(".searchResult");
+const moreButton = document.querySelector(".more");
+const movieDetails = document.querySelector(".movie_details");
+const closeBtn = document.querySelector(".close_btn");
+
 const options = {
   method: "GET",
   headers: {
@@ -8,6 +13,7 @@ const options = {
     "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com",
   },
 };
+
 searchBtn.addEventListener("click", () => {
   const searchInputText = searchInput.value.trim();
   if (!searchInputText) return;
@@ -17,18 +23,79 @@ searchBtn.addEventListener("click", () => {
   )
     .then((response) => response.json())
     .then((data) => {
-      data.d.forEach((movie) => {
-        const movies = `
-        <div class="image">
-            <img
-                src="${movie.i.imageUrl}"
-                    alt=""
-                srcset=""
-            />
-            <figcaption>${movie.l}</figcaption>
-        </div>`;
-        movieContainer.innerHTML += movies;
-      });
+      console.log(data.d);
+
+      if (data.d) {
+        let html = `<p>Search result:</p> `;
+        searchResult.innerHTML = html;
+        data.d.forEach((movie) => {
+          if (movie.y === undefined) return;
+          const movies = `
+            
+            <div class="image" id="${movie.id}">
+                <img
+                    src="${movie.i.imageUrl}"
+                        alt=""
+                    srcset=""
+                />
+                
+                <ul>
+                    <li>${movie.l}</li>
+                    <li>${movie.y}</li>
+                    <li><a href="" class="more">more...</a></li>
+                </ul>
+            </div>`;
+          movieContainer.innerHTML += movies;
+        });
+      } else if (data.d === []) {
+        const word = `Sorry couldn't find anything on ${searchInput}`;
+        movieContainer.innerHTML = word;
+      }
     })
     .catch((err) => console.error(err));
+});
+
+movieContainer.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (!e.target.classList.contains("more")) return;
+  const imageDivId = document.querySelector(".image");
+  console.log(imageDivId);
+  fetch(
+    `https://online-movie-database.p.rapidapi.com/title/get-overview-details?tconst=${imageDivId.id}&currentCountry=US`,
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const title = data.title.title;
+      const movieType = data.title.titleType;
+      const rating = data.ratings.rating;
+      const genre = data.genres.toString();
+      const summary = data.plotSummary.text;
+      let html = `
+        
+            <div class="close_btn_container">
+                <button class="close_btn"><i class="fas fa-times"></i></button>
+            </div>
+            <div class="movieContent">
+                <h3>${title}</h3>
+
+                <ul>
+                    <li>Type: ${movieType}</li>
+                    <li>Ratings: ${rating}</li>
+                    <li>Genre: ${genre}</li>
+                    <li>Summary: ${summary}</li>
+                </ul>
+            </div>
+        `;
+
+      movieDetails.innerHTML = html;
+      movieDetails.classList.add("showDetails");
+    })
+    .catch((err) => console.error(err));
+});
+
+movieDetails.addEventListener("click", (e) => {
+  if (e.target.classList.contains("fas")) {
+    movieDetails.classList.remove("showDetails");
+  }
 });
